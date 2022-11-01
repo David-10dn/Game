bb2c87c68b902c63f543201ea9e6ed94cfe5e920
daco <david.nevistic@gmail.com>
2022-11-01 21:44:48 +0100

    changed to Europe

--- Game.java
+++ Game.java
@@ -1,5 +1,15 @@
+import java.io.Console;
+
 public class Game {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Console console = System.console();
+        String answer = console.readLine("Which continent does Sweden belong to? ");
+        String continent= "Europe";
+        if(answer.equals(continent)) {
+            System.out.println("Correct!");
+        }
+        else {
+            System.out.println("No, the answer is " + continent);
+        }
     }
 }
